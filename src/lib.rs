f4b0eb8231d245dfae89e7082b0a646f7c2bbdfa
Bram van den Heuvel <bram@vandenheuvel.online>
2021-08-01 14:00:35 +0200

    Add intersecting sparse merge function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #![feature(drain_filter)]
 #![feature(is_sorted)]
+#![feature(label_break_value)]
 
-use std::collections::HashSet;
 use std::cmp::Ordering;
+use std::collections::HashSet;
+use std::mem::swap;
 
 /// Reduce the size of the vector by removing values.
 ///
@@ -103,18 +105,18 @@ pub fn merge_sparse_indices<I: Ord, T: Eq, U>(
                 let (index, value) = left.next().unwrap();
 
                 (index, operation_left(value))
-            },
+            }
             Ordering::Equal => {
                 let (left_index, left_value) = left.next().unwrap();
                 let (_right_index, right_value) = right.next().unwrap();
 
                 (left_index, operation(left_value, right_value))
-            },
+            }
             Ordering::Greater => {
                 let (index, value) = right.next().unwrap();
 
                 (index, operation_right(value))
-            },
+            }
         };
 
         if is_not_default(&result) {
@@ -132,12 +134,100 @@ pub fn merge_sparse_indices<I: Ord, T: Eq, U>(
     results
 }
 
+/// Collect two sparse iterators by merging the indices that they share, dropping the others.
+///
+/// # Arguments
+///
+/// * `left`: The first iterator, sorted by index.
+/// * `right`: The second iterator, sorted by index.
+/// * `operation`: A binary operation applied when an element is found at some index in both
+/// iterators.
+/// * `is_not_default`: A function specifying whether the result if the binary operation is the
+/// default value (that is often zero).
+///
+/// # Return value
+///
+/// A vector with sparse elements by sorted and unique index.
+pub fn merge_sparse_indices_intersect<I: Ord, T, U>(
+    mut left: impl Iterator<Item=(I, T)>,
+    mut right: impl Iterator<Item=(I, T)>,
+    operation: impl Fn(T, T) -> U,
+    is_not_default: impl Fn(&U) -> bool,
+) -> Vec<(I, U)> {
+    if let Some((mut left_index, mut left_value)) = left.next() {
+        let mut results = Vec::new();
+
+        while let Some((right_index, right_value)) = right.next() {
+            match right_index.cmp(&left_index) {
+                Ordering::Less => {}
+                Ordering::Equal => {
+                    if let Some((mut left_next_index, mut new_left_value)) = left.next() {
+                        let (left_old_index, left_old_value) = {
+                            swap(&mut left_index, &mut left_next_index);
+                            swap(&mut left_value, &mut new_left_value);
+
+                            (left_next_index, new_left_value)
+                        };
+
+                        let result = operation(left_old_value, right_value);
+                        if is_not_default(&result) {
+                            results.push((left_old_index, result));
+                        }
+                    } else {
+                        let result = operation(left_value, right_value);
+                        if is_not_default(&result) {
+                            results.push((left_index, result));
+                        }
+                        break;
+                    }
+                }
+                Ordering::Greater => {
+                    'scope: {
+                        while let Some((left_next_index, new_left_value)) = left.next() {
+                            match left_next_index.cmp(&right_index) {
+                                Ordering::Less => {}
+                                Ordering::Equal => {
+                                    let result = operation(new_left_value, right_value);
+                                    if is_not_default(&result) {
+                                        results.push((left_next_index, result));
+                                    }
+                                    if let Some((left_next_index, new_left_value)) = left.next() {
+                                        left_index = left_next_index;
+                                        left_value = new_left_value;
+                                        break 'scope;
+                                    } else {
+                                        return results;
+                                    }
+                                }
+                                Ordering::Greater => {
+                                    left_index = left_next_index;
+                                    left_value = new_left_value;
+                                    break 'scope;
+                                }
+                            }
+                        }
+
+                        return results;
+                    }
+                }
+            }
+        }
+
+        results
+    } else {
+        Vec::with_capacity(0)
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{remove_indices, remove_sparse_indices, merge_sparse_indices};
-    use std::ops::Add;
     use std::convert::identity;
+    use std::iter::empty;
+    use std::ops::Add;
+
     use relp_num::NonZero;
+
+    use crate::{merge_sparse_indices, merge_sparse_indices_intersect, remove_indices, remove_sparse_indices};
 
     #[test]
     fn test_remove_indices() {
@@ -150,7 +240,7 @@ mod test {
         assert_eq!(v, vec![0f64, 0f64]);
 
         let mut v = vec![0f64, 0f64, 2f64, 3f64, 0f64, 5f64, 0f64, 0f64, 0f64, 9f64];
-        remove_indices(&mut v,&vec![3, 4, 6]);
+        remove_indices(&mut v, &vec![3, 4, 6]);
         assert_eq!(v, vec![0f64, 0f64, 2f64, 5f64, 0f64, 0f64, 9f64]);
 
         let mut v = vec![0f64];
@@ -271,5 +361,72 @@ mod test {
         );
         let expected = vec![(1, 6), (2, 9), (3, 4)];
         assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_merge_sparse_indices_intersect() {
+        let result: Vec<(usize, i32)> = merge_sparse_indices_intersect(
+            empty(),
+            empty(),
+            |x: i32, y| x * y,
+            |x| *x != 0,
+        );
+        assert_eq!(result, vec![]);
+
+        let result = merge_sparse_indices_intersect(
+            empty(),
+            [(0, 1)].into_iter(),
+            |x, y| x * y,
+            |x| *x != 0,
+        );
+        assert_eq!(result, vec![]);
+
+        let result = merge_sparse_indices_intersect(
+            [(0, 1)].into_iter(),
+            empty(),
+            |x, y| x * y,
+            |x| *x != 0,
+        );
+        assert_eq!(result, vec![]);
+
+        let result = merge_sparse_indices_intersect(
+            [(0, 1), (2, 1), (3, 1)].into_iter(),
+            [(0, 1), (1, 2), (2, 4), (3, 8)].into_iter(),
+            |x, y| x * y,
+            |x| *x != 0,
+        );
+        assert_eq!(result, vec![(0, 1), (2, 4), (3, 8)]);
+
+        let result = merge_sparse_indices_intersect(
+            [(0, 1), (1, 2), (2, 4), (3, 8)].into_iter(),
+            [(0, 1), (2, 1), (3, 1)].into_iter(),
+            |x, y| x * y,
+            |x| *x != 0,
+        );
+        assert_eq!(result, vec![(0, 1), (2, 4), (3, 8)]);
+
+        let result = merge_sparse_indices_intersect(
+            [(0, 1), (1, 2), (3, 8)].into_iter(),
+            [(2, 1), (3, 1)].into_iter(),
+            |x, y| x * y,
+            |x| *x != 0,
+        );
+        assert_eq!(result, vec![(3, 8)]);
+
+        let result = merge_sparse_indices_intersect(
+            [(0, 1), (1, 2), (3, 8)].into_iter(),
+            [(0, 1), (2, 1), (4, 1)].into_iter(),
+            |x, y| x * y,
+            |x| *x != 0,
+        );
+        assert_eq!(result, vec![(0, 1)]);
+
+        let result = merge_sparse_indices_intersect(
+            [(0, 1), (1, 2), (3, 8), (5, 7)].into_iter(),
+            [(0, 1), (2, 1), (4, 1), (5, 7)].into_iter(),
+            |x, y| x * y,
+            |x| *x != 0,
+        );
+        assert_eq!(result, vec![(0, 1), (5, 49)]);
     }
 }
