03f3e9568b95e6602f40b14c2aa2f7e012e28bfb
Bram van den Heuvel <bram@vandenheuvel.online>
2021-08-08 21:23:00 +0200

    Add sparse slice inner product

--- src/num.rs
+++ src/num.rs
@@ -77,9 +77,76 @@ where
     total
 }
 
+/// Calculate the inner product between two vectors.
+///
+/// # Arguments
+///
+/// * `left`: The first slice, sorted by index.
+/// * `right`: The second slice, sorted by index.
+///
+/// # Return value
+///
+/// The inner product.
+#[must_use]
+pub fn inner_product_slice<'a, I: Ord, T1, T2, O>(left: &'a [(I, T1)], right: &'a [(I, T2)]) -> O
+where
+    &'a T1: Mul<&'a T2, Output=O>,
+    O: num_traits::Zero + AddAssign,
+{
+    debug_assert!(left.is_sorted_by(|a, b| Some(a.0.cmp(&b.0))));
+    debug_assert!(left.windows(2).all(|w| w[0].0 < w[1].0));
+    debug_assert!(right.is_sorted_by(|a, b| Some(a.0.cmp(&b.0))));
+    debug_assert!(right.windows(2).all(|w| w[0].0 < w[1].0));
+
+    let mut total = O::zero();
+
+    let mut left_lowest = 0;
+    let mut right_lowest = 0;
+
+    while left_lowest < left.len() && right_lowest < right.len() {
+        let self_sought = &left[left_lowest].0;
+        let other_sought = &right[right_lowest].0;
+        match self_sought.cmp(other_sought) {
+            Ordering::Less => {
+                match left[left_lowest..].binary_search_by_key(&other_sought, |(i, _)| i) {
+                    Err(diff) => {
+                        left_lowest += diff;
+                        right_lowest += 1;
+                    },
+                    Ok(diff) => {
+                        total += &left[left_lowest + diff].1 * &right[right_lowest].1;
+                        left_lowest += diff + 1;
+                        right_lowest += 1;
+                    },
+                }
+            },
+            Ordering::Greater => {
+                match right[right_lowest..].binary_search_by_key(&self_sought, |(i, _)| i) {
+                    Err(diff) => {
+                        left_lowest += 1;
+                        right_lowest += diff;
+                    },
+                    Ok(diff) => {
+                        total += &left[left_lowest].1 * &right[right_lowest + diff].1;
+                        left_lowest += 1;
+                        right_lowest += diff + 1;
+                    },
+                }
+            },
+            Ordering::Equal => {
+                total += &left[left_lowest].1 * &right[right_lowest].1;
+                left_lowest += 1;
+                right_lowest += 1;
+            },
+        }
+    }
+
+    total
+}
+
 #[cfg(test)]
 mod test {
-    use crate::num::inner_product;
+    use crate::num::{inner_product, inner_product_slice};
     use std::iter::empty;
 
     #[test]
@@ -160,6 +227,30 @@ mod test {
             inner_product(
                 [(0, 1), (3, 1), (12, 1), (13, 1)].into_iter(),
                 [(0, -1), (1, 1), (2, 1), (12, 1)].into_iter(),
+            ),
+            0,
+        );
+    }
+
+    #[test]
+    fn test_inner_product_slice() {
+        assert_eq!(inner_product_slice::<usize, i32, i32, _>(&[], &[]), 0);
+        assert_eq!(inner_product_slice(&[], &[(1, 5), (2, 7)]), 0);
+        assert_eq!(inner_product_slice::<_, _, i32, _>(&[(1, 5), (2, 7)], &[]), 0);
+        assert_eq!(inner_product_slice(&[(0, 3)], &[(0, 5)]), 3 * 5);
+        assert_eq!(inner_product_slice(&[(1, 3)], &[(1, 5)]), 3 * 5);
+        assert_eq!(inner_product_slice(&[(0, 3)], &[(1, 5)]), 0);
+        assert_eq!(inner_product_slice(&[(1, 3)], &[(0, 5)]), 0);
+        assert_eq!(inner_product_slice(&[(1, 5), (2, 6)], &[(1, 5), (2, 6)]), 5 * 5 + 6 * 6);
+        assert_eq!(inner_product_slice(&[(1, 2), (2, 3)], &[(1, 5), (2, 7)]), 2 * 5 + 3 * 7);
+        assert_eq!(inner_product_slice(&[(2, 3)], &[(0, 5), (1, 7)]), 0);
+        assert_eq!(inner_product_slice(&[(0, 3)], &[(0, 5), (1, 7)]), 3 * 5);
+        assert_eq!(inner_product_slice(&[(1, 3)], &[(0, 5), (1, 7)]), 3 * 7);
+        assert_eq!(inner_product_slice(&[(1, 3)], &[(0, 5), (2, 7)]), 0);
+        assert_eq!(
+            inner_product_slice(
+            &[(0, 1), (3, 1), (13, 1), (14, 1)],
+            &[(0, -1), (1, 1), (2, 1), (13, 1)],
             ),
             0,
         );
